9a0577f40b18d79fb86d888076e548ffb080d248
fcoconcha <fco7concha@gmail.com>
2015-02-23 00:17:36 -0800

    bonjour

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction($page)
     {
-        return $this->render('default/'.$page.'.html.twig');
+        return $this->render('AppBundle:default:'.$page.'.html.twig');
     }
 }
---
+++ src/AppBundle/Resources/public/js/show-hide.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){ 
+   $('#MEplan').on('click',function(){
+      $('#MEplan1').toggle();
+   });
+});
+
+$(document).ready(function(){ 
+   $('#MErequisitos').on('click',function(){
+      $('#MErequisitos1').toggle();
+   });
+});
+
+$(document).ready(function(){ 
+   $('#MEapoyo').on('click',function(){
+      $('#MEapoyo1').toggle();
+   });
+});
+
+$(document).ready(function(){ 
+   $('#IIplan').on('click',function(){
+      $('#IIplan1').toggle();
+   });
+});
+
+$(document).ready(function(){ 
+   $('#IIgeneral').on('click',function(){
+      $('#IIgenera1').toggle();
+   });
+});
+
+$(document).ready(function(){ 
+   $('#IIacademica').on('click',function(){
+      $('#IIacademica1').toggle();
+   });
+});
+
+$(document).ready(function(){ 
+   $('#MElineaA').on('click',function(){
+      $('#MElineaA1').toggle();
+   });
+});
+
+$(document).ready(function(){ 
+   $('#MElineaB').on('click',function(){
+      $('#MElineaB1').toggle();
+   });
+});
+
