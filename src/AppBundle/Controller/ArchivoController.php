b7bd0ba0a33919ba603a52ce17794e46040e8e6d
Buelna <andres.buelna.b@gmail.com>
2016-09-28 16:24:43 -0700

    archivos para convocatorias ordenados

--- src/AppBundle/Controller/ArchivoController.php
+++ src/AppBundle/Controller/ArchivoController.php
@@ -41,6 +41,7 @@ class ArchivoController extends Controller
 
         if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
+            $entity->upload();
             $em->persist($entity);
             $em->flush();
 
@@ -170,6 +171,7 @@ class ArchivoController extends Controller
         $editForm->handleRequest($request);
 
         if ($editForm->isValid()) {
+            $entity->upload();
             $em->flush();
 
             return $this->redirect($this->generateUrl('archivo_edit', array('id' => $id)));
